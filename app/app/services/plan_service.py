8819250535172c19fb98e3b51c5db95c0fdb9485
Derzhavin <derzhavind@bk.ru>
2021-01-22 11:37:35 +0300

    refactored plan name validation

--- app/app/services/plan_service.py
+++ app/app/services/plan_service.py
@@ -2,26 +2,33 @@ from app.daos import plan_dao
 
 
 class PlanService(object):
+    SUCCESS = {'status': 'success'}
+    PLAN_EXISTS = {'status': 'error', 'text': 'plan name exists'}
+    EMPTY_PLAN = {'status': 'error', 'text': 'empty plan name'}
 
-    def estimate_plan(self, plan_id):
-        plan = plan_dao.get(plan_id)
+    @staticmethod
+    def is_valid_plan_name(name):
+        return True if name else False
+
+    def estimate_plan(self, name):
         # some 'super' logic
+        pass
 
     def create_plan(self, name):
-        if name:
+        if self.is_valid_plan_name(name):
             if not plan_dao.exists(name):
                 plan_dao.insert(name)
-                return {'status': 'success'}
-            return {'status': 'error', 'text': 'plan name exists'}
-        return {'status': 'error', 'text': 'empty plan name'}
+                return self.SUCCESS
+            return self.PLAN_EXISTS
+        return self.EMPTY_PLAN
 
-    def rename_plan(self, name, name_new):
-        if name and name_new:
-            if not plan_dao.exists(name_new) and plan_dao.exists(name):
-                plan_dao.replace(name, name_new)
-                return {'status': 'success'}
-            return {'status': 'error', 'text': 'plan name exists'}
-        return {'status': 'error', 'text': 'empty plan name'}
+    def rename_plan(self, name, new_name):
+        if self.is_valid_plan_name(name) and self.is_valid_plan_name(new_name):
+            if not plan_dao.exists(new_name) and plan_dao.exists(name):
+                plan_dao.replace(name, new_name)
+                return self.SUCCESS
+            return self.PLAN_EXISTS
+        return self.EMPTY_PLAN
 
 
 plan_service = PlanService()
