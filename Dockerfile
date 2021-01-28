89d92199b28e606d463b0f815bcd4f2cf39e4d8d
IMConstant <kostjakov@gmail.com>
2021-01-28 20:56:36 +0300

    Dockerfile fix

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,8 @@ FROM python:3.6-alpine
 
 WORKDIR /app
 
-RUN ls
-
 COPY ./* ./
 
-RUN ls
 RUN python -m pip install -r ./requirements.txt
-
-RUN echo "Hello"
-RUN ls
 
 CMD python3 wsgi.py
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 if __name__ == '__main__':
     config = "config.Development"
     application = create_app(config)
-    application.run()
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host="0.0.0.0", port=port)
