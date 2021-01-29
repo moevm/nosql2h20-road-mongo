a057411db24225d53f3958b595e50d233efaf7ea
IMConstant <kostjakov@gmail.com>
2021-01-29 07:47:25 +0300

    add db export

--- app/app/db.py
+++ app/app/db.py
@@ -1,3 +1,18 @@
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
+
+
+def db_import_from_json(jsn):
+	pass
+
+def db_export_into_json():
+	jsn = { 'collections': [] }
+
+	for collection_name in mongo.db.list_collection_names():
+		jsn['collections'].append({
+			collection_name: list(mongo.db[collection_name].find())
+		})
+
+	return jsn
+
